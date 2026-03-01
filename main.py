ec0b593d961b7a2c17e725cfc60f2ac1ea100136
emily <frickeemily347@gmail.com>
2026-03-01 16:24:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+from engine.game import GameEngine
+
+def main():
+    print("=== Cybercity: SOC Under Attack (Terminal version) ===\n")
+    game = GameEnergy()
+    game.load_settings()
+
+    game.run()
+    if __name__ == "__main__":
+        main()
